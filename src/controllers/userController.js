d2227fe190ce9959a225e719878a43b0ec784384
Olaifa O'Darasimi Ibikunle <darasimiolaifa@gmail.com>
2019-07-12 11:44:05 +0100

    Feat(User): Implement Admin Delete
    - Write tests to test delete endpoint and run them to fail
    - Write route functions to handle the requests from the server
    - Write controller function to handle the business logic of deleting a registered user
    - Update the middleware functions to handle input validations and sanitization
    - Run test again and refactor code for elegance and speed
    
    [Delivers #167217729]

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,17 @@
 import userModel from '../models/userModel';
 import HelperUtils from '../helperUtils/helperUtils';
 
-const { getSingleUser } = userModel;
+const { getSingleUser, deleteUser } = userModel;
 
 class userController {
   static async fetchSingleUser({ params }, res) {
-    const [response] = await getSingleUser('user_id', params.user_id, false);
+    const [response] = await getSingleUser('user_id', params.userId, false);
     return HelperUtils.serverResponse(response, res);
+  }
+  
+  static async deleteOldUser({ params }, res) {
+    const response = await deleteUser(params);
+    return HelperUtils.serverResponse(response, res, 200, 'User', 'deleted');
   }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,14 @@ import userController from '../controllers/userController';
 import Authenticate from '../middleware/authenticator';
 
 export default (server) => {
-  server.route('/api/v1/users/:user_id')
+  server.route('/api/v1/users/:userId')
     .get(
       Authenticate.verifyToken,
       Authenticate.verifyOwnership,
       userController.fetchSingleUser,
+    ).delete(
+      Authenticate.verifyToken,
+      Authenticate.isAdmin,
+      userController.deleteOldUser,
     );
 };
